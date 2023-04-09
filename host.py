af68863313e9f052e46a2598dcf3077060624a8d
alvsz <102085931+alvsz@users.noreply.github.com>
2023-04-08 23:44:38 -0300

    Add files via upload

--- host.py
+++ host.py
@@ -34,6 +34,10 @@ items = {}
 def geticon(name):
     icon_theme = Gtk.IconTheme.get_default()
     icon = icon_theme.lookup_icon_for_scale(name, 64, 1, Gtk.IconLookupFlags.FORCE_SVG)
+    
+    if icon == None:
+        icon = icon_theme.lookup_icon_for_scale("computer", 64, 1, Gtk.IconLookupFlags.FORCE_SVG)
+
     # path = Gtk.IconInfo.get_filename(icon)
     return icon.get_filename()
 
